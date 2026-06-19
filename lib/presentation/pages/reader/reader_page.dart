2cd1bd4ef4e73d53ad000abbe1d5df1c43b2f017
tomcat117853 <128963662+tomcat117853@users.noreply.github.com>
2026-06-19 16:37:03 +0800

    feat: 更新阅读器核心逻辑和页面

--- lib/presentation/pages/reader/reader_page.dart
+++ lib/presentation/pages/reader/reader_page.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
+
+import '../../providers.dart';
+import '../bookshelf/bookshelf_page.dart';
 import 'reader_provider.dart';
 import 'widgets/page_text_view.dart';
-import 'widgets/scroll_text_view.dart';
-import 'widgets/reader_menu.dart';
 import 'widgets/reader_settings.dart';
-import 'widgets/chapter_list_sheet.dart';
+import 'widgets/reader_toc.dart';
 
-/// 阅读器页面 - 全屏沉浸式阅读
 class ReaderPage extends ConsumerStatefulWidget {
   final String bookId;
 
@@ -20,210 +18,114 @@ class ReaderPage extends ConsumerStatefulWidget {
 }
 
 class _ReaderPageState extends ConsumerState<ReaderPage> {
-  final FocusNode _focusNode = FocusNode();
+  bool _showSettings = false;
+  bool _showToc = false;
 
   @override
   void initState() {
     super.initState();
-    _enterImmersiveMode();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(readerProvider.notifier).loadBook(widget.bookId);
+    });
   }
 
-  @override
-  void dispose() {
-    _exitImmersiveMode();
-    _focusNode.dispose();
-    super.dispose();
+  void _toggleSettings() {
+    setState(() => _showSettings = !_showSettings);
   }
 
-  /// 进入沉浸式模式
-  void _enterImmersiveMode() {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
+  void _toggleToc() {
+    setState(() => _showToc = !_showToc);
   }
 
-  /// 退出沉浸式模式
-  void _exitImmersiveMode() {
-    SystemChrome.setEnabledSystemUIMode(
-      SystemUiMode.edgeToEdge,
-    );
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-      DeviceOrientation.landscapeLeft,
-      DeviceOrientation.landscapeRight,
-    ]);
+  void _onThemeChanged(dynamic theme) {
+    ref.read(readerProvider.notifier).setTheme(theme);
+    setState(() => _showSettings = false);
   }
 
-  /// 处理点击事件
-  void _handleTap(TapDownDetails details, ReaderState state) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
-    final tapX = details.localPosition.dx;
-    final tapY = details.localPosition.dy;
+  void _onConfigChanged(dynamic config) {
+    ref.read(readerProvider.notifier).updateLayoutConfig(config);
+  }
 
-    // 中间区域点击 - 呼出/隐藏菜单
-    final isCenterArea = tapX > screenWidth * 0.3 && tapX < screenWidth * 0.7;
-    if (isCenterArea) {
-      ref.read(readerProvider(widget.bookId).notifier).toggleMenu();
-      return;
-    }
-
-    // 左侧1/3 - 上一页
-    if (tapX < screenWidth * 0.3) {
-      ref.read(readerProvider(widget.bookId).notifier).previousPage();
-      return;
-    }
-
-    // 右侧1/3 - 下一页
-    if (tapX > screenWidth * 0.7) {
-      ref.read(readerProvider(widget.bookId).notifier).nextPage();
-      return;
-    }
+  void _onChapterSelected(int index) {
+    ref.read(readerProvider.notifier).jumpToChapter(index);
+    setState(() => _showToc = false);
   }
 
   @override
   Widget build(BuildContext context) {
-    final state = ref.watch(readerProvider(widget.bookId));
+    final state = ref.watch(readerProvider);
 
     return Scaffold(
-      backgroundColor: state.readerTheme.backgroundColor,
-      body: SafeArea(
-        top: false,
-        bottom: false,
-        child: Stack(
-          children: [
-            // 阅读内容区域
-            GestureDetector(
-              onTapDown: (details) => _handleTap(details, state),
-              child: state.isLoading
-                  ? Center(
-                      child: CircularProgressIndicator(
-                        color: state.readerTheme.textColor.withValues(alpha: 0.5),
-                      ),
-                    )
-                  : state.readMode == 0
-                      ? PageTextView(
-                          text: state.currentPageText,
-                          theme: state.readerTheme,
-                          layoutConfig: state.layoutConfig,
-                          currentPage: state.currentPageIndex + 1,
-                          totalPages: state.totalPages,
-                          chapterTitle: state.chapters.isNotEmpty
-                              ? state.chapters[state.currentChapterIndex].title
-                              : '',
-                        )
-                      : ScrollTextView(
-                          text: state.currentPageText,
-                          theme: state.readerTheme,
-                          layoutConfig: state.layoutConfig,
-                          chapterTitle: state.chapters.isNotEmpty
-                              ? state.chapters[state.currentChapterIndex].title
-                              : '',
-                        ),
-            ),
-
-            // 阅读菜单
-            if (state.showMenu)
-              ReaderMenu(
-                book: state.book,
-                currentChapter: state.currentChapterIndex,
-                totalChapters: state.chapters.length,
-                currentPage: state.currentPageIndex + 1,
-                totalPages: state.totalPages,
-                readerTheme: state.readerTheme,
-                onBack: () {
-                  ref.read(readerProvider(widget.bookId).notifier).toggleMenu();
-                  context.pop();
-                },
-                onToggleMenu: () {
-                  ref
-                      .read(readerProvider(widget.bookId).notifier)
-                      .toggleMenu();
-                },
-                onShowSettings: () => _showSettingsSheet(state),
-                onShowChapterList: () => _showChapterList(state),
-                onPreviousChapter: () {
-                  ref
-                      .read(readerProvider(widget.bookId).notifier)
-                      .previousChapter();
-                },
-                onNextChapter: () {
-                  ref
-                      .read(readerProvider(widget.bookId).notifier)
-                      .nextChapter();
-                },
-                onToggleTheme: () {
-                  ref
-                      .read(readerProvider(widget.bookId).notifier)
-                      .cycleTheme();
-                },
-                onToggleMode: () {
-                  ref
-                      .read(readerProvider(widget.bookId).notifier)
-                      .toggleReadMode();
-                },
-              ),
-
-            // 设置面板
-            if (state.showMenu)
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: ReaderSettings(
-                  theme: state.readerTheme,
-                  layoutConfig: state.layoutConfig,
-                  onIncreaseFontSize: () {
-                    ref
-                        .read(readerProvider(widget.bookId).notifier)
-                        .increaseFontSize();
+      body: state.isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Stack(
+              children: [
+                GestureDetector(
+                  onTap: _toggleSettings,
+                  onHorizontalDragEnd: (details) {
+                    if (details.primaryVelocity! > 0) {
+                      ref.read(readerProvider.notifier).prevPage();
+                    } else if (details.primaryVelocity! < 0) {
+                      ref.read(readerProvider.notifier).nextPage();
+                    }
                   },
-                  onDecreaseFontSize: () {
-                    ref
-                        .read(readerProvider(widget.bookId).notifier)
-                        .decreaseFontSize();
-                  },
-                  onIncreaseLineHeight: () {
-                    ref
-                        .read(readerProvider(widget.bookId).notifier)
-                        .increaseLineHeight();
-                  },
-                  onDecreaseLineHeight: () {
-                    ref
-                        .read(readerProvider(widget.bookId).notifier)
-                        .decreaseLineHeight();
-                  },
+                  child: PageTextView(
+                    text: state.currentPageText,
+                    layoutConfig: state.layoutConfig,
+                    theme: state.theme,
+                  ),
                 ),
-              ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  /// 显示设置面板
-  void _showSettingsSheet(ReaderState state) {
-    // 设置面板已集成在底部
-  }
-
-  /// 显示章节列表
-  void _showChapterList(ReaderState state) {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      backgroundColor: Colors.transparent,
-      builder: (context) => ChapterListSheet(
-        chapters: state.chapters,
-        currentChapterIndex: state.currentChapterIndex,
-        theme: state.readerTheme,
-        onChapterTap: (index) {
-          Navigator.pop(context);
-          ref.read(readerProvider(widget.bookId).notifier).jumpToChapter(index);
-        },
-      ),
+                if (_showSettings)
+                  Positioned(
+                    right: 0,
+                    top: 0,
+                    bottom: 0,
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    child: ReaderSettings(
+                      config: state.layoutConfig,
+                      theme: state.theme,
+                      onConfigChanged: _onConfigChanged,
+                      onThemeChanged: _onThemeChanged,
+                    ),
+                  ),
+                if (_showToc)
+                  Positioned(
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    child: ReaderToc(
+                      chapters: state.chapters,
+                      currentIndex: state.currentChapterIndex,
+                      onChapterSelected: _onChapterSelected,
+                    ),
+                  ),
+                Positioned(
+                  bottom: 20,
+                  left: 0,
+                  right: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.list),
+                        color: state.theme.textColor,
+                        onPressed: _toggleToc,
+                      ),
+                      Text(
+                        '${state.currentPage + 1} / ${state.totalPages}',
+                        style: TextStyle(color: state.theme.textColor),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.settings),
+                        color: state.theme.textColor,
+                        onPressed: _toggleSettings,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
     );
   }
 }
--- lib/presentation/pages/reader/reader_provider.dart
+++ lib/presentation/pages/reader/reader_provider.dart
@@ -1,334 +1,250 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../../domain/models/book.dart';
-import '../../../../domain/models/chapter.dart';
-import '../../../../domain/models/layout_config.dart';
-import '../../../../domain/models/reader_theme.dart';
-import '../../../../domain/models/read_progress.dart';
-import '../../../../domain/services/read_engine.dart';
-import '../../../../providers.dart';
+import '../../../domain/models/chapter.dart';
+import '../../../domain/models/layout_config.dart';
+import '../../../domain/models/read_progress.dart';
+import '../../../domain/models/reader_theme.dart';
+import '../../../domain/services/read_engine.dart';
 
-/// 阅读器状态
-class ReaderState {
-  final String bookId;
-  final Book? book;
-  final List<Chapter> chapters;
-  final int currentChapterIndex;
-  final int currentPageIndex;
-  final int totalPages;
-  final String currentPageText;
-  final bool isLoading;
-  final bool showMenu;
-  final int readMode; // 0=翻页, 1=滚动
-  final ReaderTheme readerTheme;
-  final LayoutConfig layoutConfig;
-  final ReadProgress? progress;
-  final double scrollOffset;
+final readerProvider = NotifierProvider<ReaderProvider, ReaderState>(
+  () => throw UnimplementedError(),
+);
 
-  ReaderState({
-    required this.bookId,
-    this.book,
-    this.chapters = const [],
-    this.currentChapterIndex = 0,
-    this.currentPageIndex = 0,
-    this.totalPages = 0,
-    this.currentPageText = '',
-    this.isLoading = true,
-    this.showMenu = false,
-    this.readMode = 0,
-    ReaderTheme? readerTheme,
-    LayoutConfig? layoutConfig,
-    this.progress,
-    this.scrollOffset = 0.0,
-  })  : readerTheme = readerTheme ?? ReaderTheme.presets[0],
-        layoutConfig = layoutConfig ?? LayoutConfig();
-
-  ReaderState copyWith({
-    Book? book,
-    List<Chapter>? chapters,
-    int? currentChapterIndex,
-    int? currentPageIndex,
-    int? totalPages,
-    String? currentPageText,
-    bool? isLoading,
-    bool? showMenu,
-    int? readMode,
-    ReaderTheme? readerTheme,
-    LayoutConfig? layoutConfig,
-    ReadProgress? progress,
-    double? scrollOffset,
-  }) {
-    return ReaderState(
-      bookId: bookId,
-      book: book ?? this.book,
-      chapters: chapters ?? this.chapters,
-      currentChapterIndex: currentChapterIndex ?? this.currentChapterIndex,
-      currentPageIndex: currentPageIndex ?? this.currentPageIndex,
-      totalPages: totalPages ?? this.totalPages,
-      currentPageText: currentPageText ?? this.currentPageText,
-      isLoading: isLoading ?? this.isLoading,
-      showMenu: showMenu ?? this.showMenu,
-      readMode: readMode ?? this.readMode,
-      readerTheme: readerTheme ?? this.readerTheme,
-      layoutConfig: layoutConfig ?? this.layoutConfig,
-      progress: progress ?? this.progress,
-      scrollOffset: scrollOffset ?? this.scrollOffset,
-    );
-  }
-}
-
-/// 阅读器状态通知器
-class ReaderNotifier extends StateNotifier<ReaderState> {
-  final Ref _ref;
+class ReaderProvider extends Notifier<ReaderState> {
   late final ReadEngine _readEngine;
 
-  ReaderNotifier(String bookId, this._ref)
-      : super(ReaderState(bookId: bookId, isLoading: true)) {
-    // 创建阅读引擎实例
-    final bookRepository = _ref.read(bookRepositoryProvider);
-    _readEngine = ReadEngine(bookRepository);
-
-    // 设置阅读引擎回调
-    _setupCallbacks();
-
-    _init(bookId);
+  @override
+  ReaderState build() {
+    _readEngine = ref.read(readEngineProvider);
+    return ReaderState(
+      chapters: [],
+      currentChapterIndex: 0,
+      currentPage: 0,
+      totalPages: 0,
+      currentPageText: '',
+      layoutConfig: LayoutConfig.defaultConfig(),
+      theme: ReaderTheme.day(),
+      isLoading: false,
+    );
   }
 
-  /// 设置阅读引擎回调
-  void _setupCallbacks() {
-    // 进度变化回调
-    _readEngine.onProgressChanged = (progress) {
-      if (!mounted) return;
-      state = state.copyWith(progress: progress);
-    };
-
-    // 分页完成回调
-    _readEngine.onPaginationComplete = (pageRanges) {
-      if (!mounted) return;
-      final pageText = _readEngine.getCurrentPageText();
-      state = state.copyWith(
-        totalPages: pageRanges.length,
-        currentPageText: pageText,
-        currentPageIndex: _readEngine.currentChapterIndex == state.currentChapterIndex
-            ? _readEngine.currentPage - 1
-            : 0,
-      );
-    };
-  }
-
-  /// 初始化阅读器
-  Future<void> _init(String bookId) async {
+  Future<void> loadBook(String bookId) async {
+    state = state.copyWith(isLoading: true);
     try {
-      final bookRepository = _ref.read(bookRepositoryProvider);
-
-      // 加载书籍信息
-      final book = await bookRepository.getBookById(bookId);
-      if (book == null) {
+      final chapters = await _readEngine.loadBook(bookId);
+      if (chapters.isEmpty) {
         state = state.copyWith(
+          chapters: [],
+          currentChapterIndex: 0,
+          currentPage: 0,
+          totalPages: 0,
+          currentPageText: '',
           isLoading: false,
-          currentPageText: '书籍不存在',
         );
         return;
       }
 
-      // 加载章节列表
-      final chapters = await bookRepository.getChaptersByBookId(bookId);
+      await _readEngine.loadChapter(chapters[0].id);
+      await _readEngine.updateLayoutConfig(state.layoutConfig);
+
+      final totalPages = _readEngine.getTotalPages();
+      final pageText = _readEngine.getPageText(0);
 
       state = state.copyWith(
-        book: book,
         chapters: chapters,
-      );
-
-      // 通过阅读引擎初始化阅读（自动恢复进度）
-      await _readEngine.initReading(bookId, config: state.layoutConfig);
-
-      // 同步阅读引擎状态到 UI
-      final pageText = _readEngine.getCurrentPageText();
-      state = state.copyWith(
-        isLoading: false,
-        currentChapterIndex: _readEngine.currentChapterIndex,
-        currentPageIndex: _readEngine.currentPage - 1,
-        totalPages: _readEngine.totalPages,
+        currentChapterIndex: 0,
+        currentPage: 0,
+        totalPages: totalPages,
         currentPageText: pageText,
+        isLoading: false,
       );
     } catch (e) {
-      debugPrint('初始化阅读器失败: $e');
-      state = state.copyWith(
-        isLoading: false,
-        currentPageText: '加载失败: $e',
-      );
+      state = state.copyWith(isLoading: false);
+      rethrow;
     }
   }
 
-  /// 加载章节
-  Future<void> loadChapter(int chapterIndex) async {
-    if (chapterIndex < 0 || chapterIndex >= state.chapters.length) return;
-
-    state = state.copyWith(
-      isLoading: true,
-      currentChapterIndex: chapterIndex,
-      currentPageIndex: 0,
-    );
-
-    try {
-      // 通过阅读引擎加载章节并执行分页
-      await _readEngine.loadChapter(chapterIndex);
-
-      // 获取分页后的页面文本
-      final pageText = _readEngine.getCurrentPageText();
-
-      state = state.copyWith(
-        isLoading: false,
-        currentPageText: pageText,
-        totalPages: _readEngine.totalPages,
-        currentPageIndex: 0,
-      );
-    } catch (e) {
-      debugPrint('加载章节失败: $e');
-      state = state.copyWith(
-        isLoading: false,
-        currentPageText: '章节加载失败',
-      );
-    }
+  Future<void> setTheme(ReaderTheme theme) async {
+    state = state.copyWith(theme: theme);
   }
 
-  /// 翻到下一页
-  void nextPage() {
-    final moved = _readEngine.nextPage();
-    if (moved) {
-      // 阅读引擎成功翻页，更新 UI 状态
-      final pageText = _readEngine.getCurrentPageText();
-      state = state.copyWith(
-        currentPageIndex: _readEngine.currentPage - 1,
-        currentPageText: pageText,
-      );
-    } else {
-      // 已到本章末尾，翻到下一章
-      nextChapter();
-    }
-  }
-
-  /// 翻到上一页
-  void previousPage() {
-    final moved = _readEngine.previousPage();
-    if (moved) {
-      // 阅读引擎成功翻页，更新 UI 状态
-      final pageText = _readEngine.getCurrentPageText();
-      state = state.copyWith(
-        currentPageIndex: _readEngine.currentPage - 1,
-        currentPageText: pageText,
-      );
-    } else {
-      // 已到本章开头，翻到上一章
-      previousChapter();
-    }
-  }
-
-  /// 下一章
-  void nextChapter() {
-    if (state.currentChapterIndex < state.chapters.length - 1) {
-      loadChapter(state.currentChapterIndex + 1);
-    }
-  }
-
-  /// 上一章
-  void previousChapter() {
-    if (state.currentChapterIndex > 0) {
-      loadChapter(state.currentChapterIndex - 1);
-    }
-  }
-
-  /// 跳转到指定页
-  void jumpToPage(int pageIndex) {
-    if (pageIndex >= 0 && pageIndex < state.totalPages) {
-      _readEngine.jumpToPage(pageIndex);
-      final pageText = _readEngine.getCurrentPageText();
-      state = state.copyWith(
-        currentPageIndex: pageIndex,
-        currentPageText: pageText,
-      );
-    }
-  }
-
-  /// 跳转到指定章节
-  Future<void> jumpToChapter(int chapterIndex) async {
-    await loadChapter(chapterIndex);
-  }
-
-  /// 切换菜单显示
-  void toggleMenu() {
-    state = state.copyWith(showMenu: !state.showMenu);
-  }
-
-  /// 切换阅读模式
-  void toggleReadMode() {
-    final newMode = state.readMode == 0 ? 1 : 0;
-    _readEngine.updateReadMode(newMode);
-    state = state.copyWith(readMode: newMode);
-  }
-
-  /// 切换主题
-  void cycleTheme() {
-    final nextIndex =
-        (state.readerTheme.themeIndex + 1) % ReaderTheme.presets.length;
-    state = state.copyWith(readerTheme: ReaderTheme.presets[nextIndex]);
-  }
-
-  /// 更新排版配置
-  void updateLayoutConfig(LayoutConfig config) async {
-    state = state.copyWith(layoutConfig: config);
-    // 通过阅读引擎更新排版并重新分页
+  Future<void> updateLayoutConfig(LayoutConfig config) async {
     await _readEngine.updateLayoutConfig(config);
-    // 分页完成后更新 UI
-    final pageText = _readEngine.getCurrentPageText();
+    final totalPages = _readEngine.getTotalPages();
+    final currentPage = _readEngine.getCurrentPage();
+    final pageText = _readEngine.getPageText(currentPage);
+
     state = state.copyWith(
-      totalPages: _readEngine.totalPages,
+      layoutConfig: config,
+      totalPages: totalPages,
+      currentPage: currentPage,
       currentPageText: pageText,
     );
   }
 
-  /// 增大字体
-  void increaseFontSize() {
-    final newConfig = state.layoutConfig.copyWith(
-      fontSize: (state.layoutConfig.fontSize + 1).clamp(12.0, 32.0),
+  Future<void> nextPage() async {
+    if (state.currentPage >= state.totalPages - 1) {
+      await nextChapter();
+      return;
+    }
+
+    final nextPage = state.currentPage + 1;
+    final pageText = _readEngine.getPageText(nextPage);
+
+    state = state.copyWith(
+      currentPage: nextPage,
+      currentPageText: pageText,
     );
-    updateLayoutConfig(newConfig);
   }
 
-  /// 减小字体
-  void decreaseFontSize() {
-    final newConfig = state.layoutConfig.copyWith(
-      fontSize: (state.layoutConfig.fontSize - 1).clamp(12.0, 32.0),
+  Future<void> prevPage() async {
+    if (state.currentPage <= 0) {
+      await prevChapter();
+      return;
+    }
+
+    final prevPage = state.currentPage - 1;
+    final pageText = _readEngine.getPageText(prevPage);
+
+    state = state.copyWith(
+      currentPage: prevPage,
+      currentPageText: pageText,
     );
-    updateLayoutConfig(newConfig);
   }
 
-  /// 增大行距
-  void increaseLineHeight() {
-    final newConfig = state.layoutConfig.copyWith(
-      lineHeight: (state.layoutConfig.lineHeight + 0.1).clamp(1.0, 3.0),
+  Future<void> nextChapter() async {
+    if (state.currentChapterIndex >= state.chapters.length - 1) {
+      return;
+    }
+
+    final nextChapterIndex = state.currentChapterIndex + 1;
+    final chapter = state.chapters[nextChapterIndex];
+
+    await _readEngine.loadChapter(chapter.id);
+    await _readEngine.updateLayoutConfig(state.layoutConfig);
+
+    final totalPages = _readEngine.getTotalPages();
+    final pageText = _readEngine.getPageText(0);
+
+    state = state.copyWith(
+      currentChapterIndex: nextChapterIndex,
+      currentPage: 0,
+      totalPages: totalPages,
+      currentPageText: pageText,
     );
-    updateLayoutConfig(newConfig);
   }
 
-  /// 减小行距
-  void decreaseLineHeight() {
-    final newConfig = state.layoutConfig.copyWith(
-      lineHeight: (state.layoutConfig.lineHeight - 0.1).clamp(1.0, 3.0),
+  Future<void> prevChapter() async {
+    if (state.currentChapterIndex <= 0) {
+      return;
+    }
+
+    final prevChapterIndex = state.currentChapterIndex - 1;
+    final chapter = state.chapters[prevChapterIndex];
+
+    await _readEngine.loadChapter(chapter.id);
+    await _readEngine.updateLayoutConfig(state.layoutConfig);
+
+    final totalPages = _readEngine.getTotalPages();
+    final pageText = _readEngine.getPageText(totalPages - 1);
+
+    state = state.copyWith(
+      currentChapterIndex: prevChapterIndex,
+      currentPage: totalPages - 1,
+      totalPages: totalPages,
+      currentPageText: pageText,
     );
-    updateLayoutConfig(newConfig);
   }
 
-  @override
-  void dispose() {
-    _readEngine.dispose();
-    super.dispose();
+  Future<void> jumpToChapter(int index) async {
+    if (index < 0 || index >= state.chapters.length) {
+      return;
+    }
+
+    final chapter = state.chapters[index];
+    await _readEngine.loadChapter(chapter.id);
+    await _readEngine.updateLayoutConfig(state.layoutConfig);
+
+    final totalPages = _readEngine.getTotalPages();
+    final pageText = _readEngine.getPageText(0);
+
+    state = state.copyWith(
+      currentChapterIndex: index,
+      currentPage: 0,
+      totalPages: totalPages,
+      currentPageText: pageText,
+    );
+  }
+
+  Future<void> jumpToPage(int page) async {
+    if (page < 0 || page >= state.totalPages) {
+      return;
+    }
+
+    final pageText = _readEngine.getPageText(page);
+    state = state.copyWith(
+      currentPage: page,
+      currentPageText: pageText,
+    );
+  }
+
+  Future<ReadProgress> getProgress() async {
+    final bookId = state.chapters.isNotEmpty ? state.chapters[0].bookId : '';
+    return ReadProgress(
+      bookId: bookId,
+      chapterIndex: state.currentChapterIndex,
+      pageIndex: state.currentPage,
+      totalPages: state.totalPages,
+      lastReadTime: DateTime.now(),
+    );
+  }
+
+  Future<void> saveProgress() async {
+    final progress = await getProgress();
+    await _readEngine.saveProgress(progress);
   }
 }
 
-/// 阅读器Provider（使用 family 传递 bookId 参数）
-final readerProvider = StateNotifierProvider.autoDispose
-    .family<ReaderNotifier, ReaderState, String>((ref, bookId) {
-  return ReaderNotifier(bookId, ref);
-});
+class ReaderState {
+  final List<Chapter> chapters;
+  final int currentChapterIndex;
+  final int currentPage;
+  final int totalPages;
+  final String currentPageText;
+  final LayoutConfig layoutConfig;
+  final ReaderTheme theme;
+  final bool isLoading;
+
+  ReaderState({
+    required this.chapters,
+    required this.currentChapterIndex,
+    required this.currentPage,
+    required this.totalPages,
+    required this.currentPageText,
+    required this.layoutConfig,
+    required this.theme,
+    required this.isLoading,
+  });
+
+  ReaderState copyWith({
+    List<Chapter>? chapters,
+    int? currentChapterIndex,
+    int? currentPage,
+    int? totalPages,
+    String? currentPageText,
+    LayoutConfig? layoutConfig,
+    ReaderTheme? theme,
+    bool? isLoading,
+  }) {
+    return ReaderState(
+      chapters: chapters ?? this.chapters,
+      currentChapterIndex: currentChapterIndex ?? this.currentChapterIndex,
+      currentPage: currentPage ?? this.currentPage,
+      totalPages: totalPages ?? this.totalPages,
+      currentPageText: currentPageText ?? this.currentPageText,
+      layoutConfig: layoutConfig ?? this.layoutConfig,
+      theme: theme ?? this.theme,
+      isLoading: isLoading ?? this.isLoading,
+    );
+  }
+}
